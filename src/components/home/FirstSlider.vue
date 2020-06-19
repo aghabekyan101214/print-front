04e528576ced3b48af3078586aaf5f9b89558dbe
unknown <aghabekyan.pargev@gmail.com>
2020-06-20 00:56:26 +0400

    added home

--- src/components/home/FirstSlider.vue
+++ src/components/home/FirstSlider.vue
@@ -1,54 +1,33 @@
 <template>
     <section class="banner full-container adaptive">
-        <carousel autoplay="autoplay" :loop=true :autoplayTimeout=5000 :per-page="1" :paginationEnabled="true" :scrollPerPage="true">
+        <carousel autoplay="autoplay" :loop=true :autoplayTimeout=3000 :per-page="1" :paginationEnabled="true" :scrollPerPage="true">
             <slide>
                 <div class="row m0">
                     <div class="col-xl-12 pl-0 pr-0">
-                        <img class="img-fluid d-none d-lg-block" src="../../assets/images/banner1.jpg" alt="First slide">
+                        <img class="img-fluid d-none d-lg-block" src="../../assets/images/home/banner1.jpg" alt="First slide">
                         <img class="img-fluid d-lg-none" src="../../assets/images/banner1-mob.jpeg" alt="First slide">
                     </div>
                 </div>
             </slide>
+
             <slide>
                 <div class="row m0">
                     <div class="col-xl-12 pl-0 pr-0">
-                        <img class="img-fluid d-none d-lg-block" src="../../assets/images/slider2_desk.jpg" alt="Second slide">
+                        <img class="img-fluid d-none d-lg-block" src="../../assets/images/home/banner2.jpg" alt="Second slide">
                         <img class="img-fluid d-lg-none" src="../../assets/images/slider2_mob.jpg" alt="Second slide">
                     </div>
                 </div>
             </slide>
-<!--            <slide>-->
-<!--                <div class="row m0">-->
-<!--                    <div class="col-lg-6 col-md-6">-->
-<!--                        <img class="img-fluid" src="../../assets/images/banner-img.png" alt="First slide">-->
-<!--                    </div>-->
-<!--                    <div class="col-lg-6">-->
-<!--                        <div class="col-lg-9 f-right-banner">-->
-<!--                            <h2 class="title top-title"><span class="blue">Business </span>Services</h2>-->
-<!--                            <p class="generic-details">-->
-<!--                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.-->
-<!--                            </p>-->
-<!--                            <a href="#" class="more-btn">Learn more</a>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </slide>-->
-<!--            <slide>-->
-<!--                <div class="row m0">-->
-<!--                    <div class="col-lg-6 col-md-6">-->
-<!--                        <img class="img-fluid" src="../../assets/images/banner-img.png" alt="First slide">-->
-<!--                    </div>-->
-<!--                    <div class="col-lg-6">-->
-<!--                        <div class="col-lg-9 f-right-banner">-->
-<!--                            <h2 class="title top-title"><span class="blue">Business </span>Services</h2>-->
-<!--                            <p class="generic-details">-->
-<!--                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.-->
-<!--                            </p>-->
-<!--                            <a href="#" class="more-btn">Learn more</a>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </slide>-->
+
+            <slide>
+                <div class="row m0">
+                    <div class="col-xl-12 pl-0 pr-0">
+                        <img class="img-fluid d-none d-lg-block" src="../../assets/images/home/banner3.jpg" alt="Third slide">
+                        <img class="img-fluid d-lg-none" src="../../assets/images/home/banner3.jpg" alt="Third slide">
+                    </div>
+                </div>
+            </slide>
+
         </carousel>
     </section>
 </template>
